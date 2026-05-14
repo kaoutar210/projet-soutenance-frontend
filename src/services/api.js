f75465c1fc6dddcdfd6fa29a92e7a869871f2a9f
kaoutar <kaoutar@gmail.com>
2026-05-15 00:00:42 +0100

    Connect React to Railway API

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
-  headers: {
+baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api",  headers: {
     Accept: "application/json",
   },
 });
